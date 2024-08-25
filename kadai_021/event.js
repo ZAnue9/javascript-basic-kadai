4f5d15d9ae7715a2e73ff7cf4394947dab82014a
ZAnue9 <uap4_si15p@icloud.com>
2024-08-25 22:07:55 +0900

    syuusei

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById("btn");
-setTimeout(() => {
-  btn.addEventListener("click", function () {
-    document.getElementById("text").innerText = "ボタンをクリックしました";
-  });
-}, 2000);
+const text = document.getElementById("text");
+btn.addEventListener("click", function () {
+  setTimeout(
+    () => {
+      text.textContent = "ボタンをクリックしました";
+    },
+
+    2000
+  );
+});
